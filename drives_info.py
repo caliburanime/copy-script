6a43270c64f641e108529303826e3e9aa1c08301
Calibur <shahoriar21@gmail.com>
2025-11-29 20:18:10 +0600

    Added function to find Drives

---
+++ drives_info.py
@@ -0,0 +1,18 @@
+import wmi
+
+# DRIVE_TYPES = {
+#     0 : "Unknown",
+#     1 : "No Root Directory",
+#     2 : "Removable Disk",
+#     3 : "Local Disk",
+#     4 : "Network Drive",
+#     5 : "Compact Disc",
+#     6 : "RAM Disk"
+# }
+
+
+def get_removeable_disk_letter ():
+    c  = wmi.WMI()
+    for drive in c.Win32_LogicalDisk():
+        if drive.DriveType == 2:
+            return drive
