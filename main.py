cceff835d5fb37d9a94ea0da17ff0d2e0760ca19
CrispyChicken01 <nabilzaman24@gmail.com>
2025-11-29 17:49:37 +0600

    added final path function

--- main.py
+++ main.py
@@ -4,13 +4,28 @@ import datetime
 
 source_dir = Path('g:/')
 
+
+def get_finalpath(the_file):
+	test_path = Path(the_file)
+
+	drive_letter = 'c:'
+	part_list = list(test_path.parent.parts)
+
+	fixed_path = part_list[1:]
+	final_path = [drive_letter] + fixed_path
+	final_path_object = Path(*final_path)
+
+	print(final_path_object)
+	pass
+
 def grab_files():
 	
 	for i in source_dir.rglob(f"*.ppt*"):
 		if any(x.startswith('.') for x in i.parts):
 			continue 	# Ignores any hidden folders (folders starting with an '.')
+		get_finalpath(i)
 		print("*" * 20 )
-		print(i)		# Outputs the grabbed files
+		# print(i)		# Outputs the grabbed files
 		# print("\n")
 
 
@@ -31,7 +46,7 @@ def main():
 	
 	
 	fold_name()
-	# grab_files()
+	grab_files()
 
 
 
